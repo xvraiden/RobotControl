25d38867c774a2cbdf37f1b5e0eea392723f68ca
BobSanders64 <58925347+BobSanders64@users.noreply.github.com>
2025-02-17 09:32:49 -0500

    Update M.py
    Added an MS option

--- Templates/M.py
+++ Templates/M.py
@@ -1,11 +1,5 @@
 import numpy as np
 import sympy as sy
-from numpy import pi as pi
-from numpy import cos as cos
-from numpy import sin as sin
-from numpy import tan as tan
-from numpy import *
-import math
 
 class M:
 
@@ -37,21 +31,21 @@ class M:
   def trans(MovementInListForm):
     if not isinstance(MovementInListForm,(np.ndarray,list)):
       return 'Make input to Trans a list'
-    translationmatrix=np.eye(3)
-    translationmatrix[0][2]=MovementInListForm[0]
-    translationmatrix[1][2]=MovementInListForm[1]
-    translationmatrix[2][2]=MovementInListForm[2]
+    translationmatrix=np.eye(4)
+    translationmatrix[0,3]=MovementInListForm[0]
+    translationmatrix[1,3]=MovementInListForm[1]
+    translationmatrix[2,3]=MovementInListForm[2]
     return translationmatrix
 
   @staticmethod
   def rot(variable,Θ):
-    Θ=Θ*pi/180
+    Θ=Θ*np.pi/180
     if variable=='x':
-      rotmatrix=np.array([[1,0,0],[0,cos(Θ),-sin(Θ)],[0,sin(Θ),cos(Θ)]])
+      rotmatrix=np.array([[1,0,0],[0,np.cos(Θ),-np.sin(Θ)],[0,np.sin(Θ),np.cos(Θ)]])
     elif variable=='y':
-      rotmatrix=np.array([[cos(Θ),0,sin(Θ)],[0,1,0],[-sin(Θ),0,cos(Θ)]])
+      rotmatrix=np.array([[np.cos(Θ),0,np.sin(Θ)],[0,1,0],[-np.sin(Θ),0,np.cos(Θ)]])
     elif variable=='z':
-      rotmatrix=np.array([[cos(Θ),-sin(Θ),0],[sin(Θ),cos(Θ),0],[0,0,1]])
+      rotmatrix=np.array([[np.cos(Θ),-np.sin(Θ),0],[np.sin(Θ),np.cos(Θ),0],[0,0,1]])
     else:
       return "Invalid rotation axis. Choose 'x','y',or 'z'."
 
@@ -66,12 +60,12 @@ class M:
 
   @staticmethod
   def EulerAngles(matrix1):
-    Θ=math.atan2(math.sqrt(1-matrix1[2][2]**2),matrix1[2][2])
-    Θ=Θ*180/pi
-    Φ=math.atan2(matrix1[1][2],matrix1[0][2])
-    Φ=Φ*180/pi
-    Ψ=math.atan2(matrix1[2][1],-matrix1[2][0])
-    Ψ=Ψ*180/pi
+    Θ=np.atan2(np.sqrt(1-matrix1[2,2]**2),matrix1[2,2])
+    Θ=Θ*180/np.pi
+    Φ=np.atan2(matrix1[1,2],matrix1[0,2])
+    Φ=Φ*180/np.pi
+    Ψ=np.atan2(matrix1[2,1],-matrix1[2,0])
+    Ψ=Ψ*180/np.pi
     return round(Θ,6),round(Φ,6),round(Ψ,6)
 
   @staticmethod
@@ -98,14 +92,13 @@ class M:
         if rots!='z' and rots!='y' and rots!='x':
           return "Please enter only 'x', 'y', and 'z'!"
 
-    biganswer=answerlist[0]
     return M.CurMult(M.CurMult(answerlist[0],answerlist[1]),answerlist[2])
     
   @staticmethod
   def homogenous(variable,Θ,MovementSetFalseIfNone):
     homgenousmatrix=np.eye(4)
 
-    if Θ!=False:
+    if Θ!=False and variable!=False:
       rotationpart=M.rot(variable,Θ)
       homgenousmatrix[:3,:3]=rotationpart
 
@@ -115,25 +108,159 @@ class M:
       else:
         homgenousmatrix[:3,3]=MovementSetFalseIfNone
     
-    if Θ==False and MovementSetFalseIfNone==False:
-      return "Don't make both 'false' conditions bro!"
+    if (Θ==False or variable==False) and MovementSetFalseIfNone==False:
+      return "Don't make both rotation and movement 'false' bro! You get an error till you fix it"
 
     return homgenousmatrix
 
   @staticmethod
   def axisfind(rotmatrix):
-    Θ = math.acos((np.trace(rotmatrix) - 1) / 2)
-    k = (1 / (2 * math.sin(np.deg2rad(Θ)))) * np.array([[rotmatrix[2, 1] - rotmatrix[1, 2]], [rotmatrix[0, 2] - rotmatrix[2, 0]], [rotmatrix[1, 0] - rotmatrix[0, 1]]])
+    Θ = np.acos((np.trace(rotmatrix) - 1) / 2)
+    k = (1 / (2 * np.sin(np.deg2rad(Θ)))) * np.array([[rotmatrix[2, 1] - rotmatrix[1, 2]], [rotmatrix[0, 2] - rotmatrix[2, 0]], [rotmatrix[1, 0] - rotmatrix[0, 1]]])
     return[k / np.linalg.norm(k), np.rad2deg(Θ)]
 
   @staticmethod
-  def inverse(matrix):
+  def inv(matrix):
     return np.linalg.inv(matrix)
 
   @staticmethod
   def DH(a,α,d,Θ):
-    row1 = np.array([math.cos(np.deg2rad(Θ)), -math.sin(np.deg2rad(Θ)) * math.cos(np.deg2rad(α)), math.sin(np.deg2rad(Θ)) * math.sin(np.deg2rad(α)), a * math.cos(np.deg2rad(Θ))])
-    row2 = np.array([math.sin(np.deg2rad(Θ)), math.cos(np.deg2rad(Θ)) * math.cos(np.deg2rad(α)), -math.cos(np.deg2rad(Θ)) * math.sin(np.deg2rad(α)), a * math.sin(np.deg2rad(Θ))])
-    row3 = np.array([0, math.sin(np.deg2rad(α)), math.cos(np.deg2rad(α)), d])
+    row1 = np.array([np.cos(np.deg2rad(Θ)), -np.sin(np.deg2rad(Θ)) * np.cos(np.deg2rad(α)), np.sin(np.deg2rad(Θ)) * np.sin(np.deg2rad(α)), a * np.cos(np.deg2rad(Θ))])
+    row2 = np.array([np.sin(np.deg2rad(Θ)), np.cos(np.deg2rad(Θ)) * np.cos(np.deg2rad(α)), -np.cos(np.deg2rad(Θ)) * np.sin(np.deg2rad(α)), a * np.sin(np.deg2rad(Θ))])
+    row3 = np.array([0, np.sin(np.deg2rad(α)), np.cos(np.deg2rad(α)), d])
     row4 = np.array([0, 0, 0, 1])
     return np.array([row1, row2, row3, row4])
+  
+class MS:
+
+  @staticmethod
+  def CurMult(matrix1,matrix2):
+    matrix1=sy.Matrix(matrix1); matrix2=sy.Matrix(matrix2)
+    return matrix1*matrix2
+
+  @staticmethod
+  def FixMult(matrix1,matrix2):
+    matrix1=sy.Matrix(matrix1); matrix2=sy.Matrix(matrix2)
+    return matrix2*matrix1
+
+  @staticmethod
+  def trans(MovementInListForm):
+    if not isinstance(MovementInListForm,(sy.Matrix,list)):
+      return 'Make input to Trans a list! Also make sure its either Python list or Sympy List since your using symbolic'
+    translationmatrix=sy.eye(4)
+    translationmatrix[0,3]=sy.sympify(MovementInListForm[0])
+    translationmatrix[1,3]=sy.sympify(MovementInListForm[1])
+    translationmatrix[2,3]=sy.sympify(MovementInListForm[2])
+    return translationmatrix
+
+  @staticmethod
+  def rot(variable,Θ):
+    Θ=sy.sympify(Θ)*sy.pi/180
+    if variable=='x':
+      rotmatrix=sy.Matrix([[1,0,0],[0,sy.cos(Θ),-sy.sin(Θ)],[0,sy.sin(Θ),sy.cos(Θ)]])
+    elif variable=='y':
+      rotmatrix=sy.Matrix([[sy.cos(Θ),0,sy.sin(Θ)],[0,1,0],[-sy.sin(Θ),0,sy.cos(Θ)]])
+    elif variable=='z':
+      rotmatrix=sy.Matrix([[sy.cos(Θ),-sy.sin(Θ),0],[sy.sin(Θ),sy.cos(Θ),0],[0,0,1]])
+    else:
+      return "Invalid rotation axis. Choose 'x','y',or 'z'."
+
+    return rotmatrix
+
+  @staticmethod
+  def EulerAngles(matrix1):
+    Θ=sy.atan2(sy.sqrt(1-matrix1[2,2]**2),matrix1[2,2])
+    Θ=Θ*180/sy.pi
+    Φ=sy.atan2(matrix1[1,2],matrix1[0,2])
+    Φ=Φ*180/sy.pi
+    Ψ=sy.atan2(matrix1[2,1],-matrix1[2,0])
+    Ψ=Ψ*180/sy.pi
+    return Θ,Φ,Ψ
+
+  @staticmethod
+  def ReverseEuler(RotationsAsList,AnglesAsList):           
+    answerlist=[]
+
+    #Checks
+    if not isinstance(RotationsAsList, (list,sy.Matrix)) or not isinstance(AnglesAsList, (list,sy.Matrix)):
+      return 'Please type both as a list! [RotationsAsList],[AnglesAsList]. Sympy lists are ok too!'
+    elif len(RotationsAsList)!=1 or len(AnglesAsList)!=1:
+      return 'Please enter both as a 1D list!'
+
+    #Actual work
+    else:
+      #Converts list into lowercase in case it was inputted wrong
+      RotationsAsList=[letters.lower() for letters in RotationsAsList]
+      for i,rots in enumerate(RotationsAsList):
+        if rots=='z':
+          answerlist.append(MS.rot('z',AnglesAsList[i]))
+        if rots=='y':
+          answerlist.append(MS.rot('y',AnglesAsList[i]))
+        if rots=='x':
+          answerlist.append(MS.rot('x',AnglesAsList[i]))
+        if rots!='z' and rots!='y' and rots!='x':
+          return "Please enter only 'x', 'y', and 'z'!"
+
+    return MS.CurMult(MS.CurMult(answerlist[0],answerlist[1]),answerlist[2])
+    
+  @staticmethod
+  def homogenous(variable,Θ,MovementSetFalseIfNone):
+    homgenousmatrix=sy.eye(4)
+
+    if Θ!=False and variable!=False:
+      rotationpart=MS.rot(variable,Θ)
+      homgenousmatrix[:3,:3]=rotationpart
+
+    if MovementSetFalseIfNone!=False:
+      if not isinstance(MovementSetFalseIfNone,(list,sy.Matrix)):
+        print('Please Type it in as [x,y,z] for the last collum.')
+      else:
+        homgenousmatrix[:3,3]=sy.sympify(MovementSetFalseIfNone)
+    
+    if (Θ==False or variable==False) and MovementSetFalseIfNone==False:
+      return "Don't make both rotation and movement 'false' bro! You get an error till you fix it"
+
+    return homgenousmatrix
+
+  @staticmethod
+  def axisfind(rotmatrix):
+    rotmatrix=sy.Matrix(rotmatrix)
+    Θ = sy.acos((rotmatrix.trace() - 1) / 2)
+    #k = (1 / (2 * np.sin(np.deg2rad(Θ)))) * np.array([[rotmatrix[2, 1] - rotmatrix[1, 2]], [rotmatrix[0, 2] - rotmatrix[2, 0]], [rotmatrix[1, 0] - rotmatrix[0, 1]]])
+    #normalize k
+    k_x = rotmatrix[2,1] - rotmatrix[1,2]
+    k_y = rotmatrix[0,2] - rotmatrix[2,0]
+    k_z = rotmatrix[1,0] - rotmatrix[0,1]
+
+    #This factor is 1/(2*sin(Theta))
+    denom = 2*sy.sin(Θ)
+    k = sy.Matrix([k_x, k_y, k_z]) / denom
+
+    #Normalized axis
+    k = k / k.norm()
+
+    #Convert Theta to degrees
+    Θ = Θ*180/sy.pi
+
+    return [k, Θ]
+
+  @staticmethod
+  def inv(matrix):
+    answer=sy.Matrix(matrix)
+    return answer.inv()
+
+  @staticmethod
+  def DH(a,α,d,Θ,r):
+    Θ=sy.sympify(Θ)
+    if r=='d':
+      Θ*sy.pi/180
+    α=sy.sympify(α)
+    if α=='d':
+      α=α*sy.pi/180
+    a=sy.sympify(a)
+    d=sy.sympify(d)
+    row1 = [sy.cos(Θ), -sy.sin(Θ) * sy.cos(α), sy.sin(Θ) * sy.sin(α), a * sy.cos(Θ)]
+    row2 = [sy.sin(Θ), sy.cos(Θ) * sy.cos(α), -sy.cos(Θ) * sy.sin(α), a * sy.sin(Θ)]
+    row3 = [0, sy.sin(α), sy.cos(α), d]
+    row4 = [0, 0, 0, 1]
+    return sy.Matrix([row1, row2, row3, row4])
